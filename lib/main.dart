ca4c753aaf0f8f5af28b6f1514d5dbdc55863b3b
ROHAN KISHOR PATIL <rohanp12326@gmail.com>
2022-11-30 23:04:32 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 1;
+  void change_go() {
+    setState(() {
+      HapticFeedback.heavyImpact();
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+      print('both button got pressed');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                change_go();
+              },
+              child: Image.asset(
+                'images/dice$leftDiceNumber.png',
+              ),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                change_go();
+              },
+              child: Image.asset(
+                'images/dice$rightDiceNumber.png',
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
